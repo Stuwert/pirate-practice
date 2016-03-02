89ca3269be626cc3f64e20fab565736bb125bf79
Stuart Urback <stuart@urback.net>
2016-03-02 11:18:45 -0700

    Wire angular apps

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,12 @@
-var app = angular.module('pirates', []);
+var app = angular.module('pirates', ['ngRoute']);
 
 app.controller('PiratesController', ['$scope', function($scope){
   $scope.test = 1 + 3;
 }])
+
+app.config(function($routeProvider){
+  $routeProvider
+    .when('/', {
+      templateUrl: '/views/pirates.html',
+    })
+})
